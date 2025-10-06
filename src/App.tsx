b27c44f1f9bf3928b5241015f9ebf7c2b517fa0f
swastikbytes-official <swastikbytes@gmail.com>
2025-10-07 03:18:48 +0530

    GTM integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider,Helmet } from 'react-helmet-async';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -58,6 +58,17 @@ function App() {
 
   return (
     <HelmetProvider>
+       <Helmet>
+        <script>
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5LTCZSFV');
+          `}
+        </script>
+      </Helmet>
       <div className="App">
         <Header />
         <main>
